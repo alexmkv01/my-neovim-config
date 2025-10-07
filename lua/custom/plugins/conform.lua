4ef804c2a2874754bee4ed83ae273f35d405f56e
Alex Mikheev <alex.mikheev@beauhurst.com>
2025-10-07 17:21:00 +0100

    (format) linting

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,61 +1,64 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
-        "<leader>f",
-        function()
-          require("conform").format({ async = true, lsp_fallback = true })
-        end,
-        mode = "",
-        desc = "Format buffer",
-      },
-    },
-    opts = {
-      formatters_by_ft = {
-        python = { "ruff_isort", "ruff_format", "ruff_fix" },
-        lua = { "stylua" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        sh = { "shfmt" },
-      },
-      format_on_save = {
-        timeout_ms = 1000,
-        lsp_fallback = true,
-      },
-      formatters = {
-        ruff_isort = {
-          command = vim.fn.expand("~/.local/share/nvim/mason/bin/ruff"),
-          args = { 
-            "check", 
-            "--select", "I,F401",  -- I for import sorting, F401 for unused imports
-            "--fix",
-            "--stdin-filename", "$FILENAME", 
-            "-" 
-          },
-          stdin = true,
-        },
-        ruff_format = {
-          command = vim.fn.expand("~/.local/share/nvim/mason/bin/ruff"),
-          args = { "format", "--stdin-filename", "$FILENAME", "-" },
-          stdin = true,
-        },
-        ruff_fix = {
-          command = vim.fn.expand("~/.local/share/nvim/mason/bin/ruff"),
-          args = { 
-            "check", 
-            "--fix",
-            "--stdin-filename", "$FILENAME", 
-            "-" 
-          },
-          stdin = true,
-        },
-      },
-    },
-  },
+	{
+		'stevearc/conform.nvim',
+		event = { 'BufWritePre' },
+		cmd = { 'ConformInfo' },
+		keys = {
+			{
+				'<leader>f',
+				function()
+					require('conform').format { async = true, lsp_fallback = true }
+				end,
+				mode = '',
+				desc = 'Format buffer',
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				python = { 'ruff_isort', 'ruff_format', 'ruff_fix' },
+				lua = { 'stylua' },
+				javascript = { 'prettier' },
+				typescript = { 'prettier' },
+				json = { 'prettier' },
+				yaml = { 'prettier' },
+				markdown = { 'prettier' },
+				sh = { 'shfmt' },
+			},
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_fallback = true,
+			},
+			formatters = {
+				ruff_isort = {
+					command = vim.fn.expand '~/.local/share/nvim/mason/bin/ruff',
+					args = {
+						'check',
+						'--select',
+						'I,F401', -- I for import sorting, F401 for unused imports
+						'--fix',
+						'--stdin-filename',
+						'$FILENAME',
+						'-',
+					},
+					stdin = true,
+				},
+				ruff_format = {
+					command = vim.fn.expand '~/.local/share/nvim/mason/bin/ruff',
+					args = { 'format', '--stdin-filename', '$FILENAME', '-' },
+					stdin = true,
+				},
+				ruff_fix = {
+					command = vim.fn.expand '~/.local/share/nvim/mason/bin/ruff',
+					args = {
+						'check',
+						'--fix',
+						'--stdin-filename',
+						'$FILENAME',
+						'-',
+					},
+					stdin = true,
+				},
+			},
+		},
+	},
 }
