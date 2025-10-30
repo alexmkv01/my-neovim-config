a91fe4d06a13bd78cebabb17d63b7eb591e1a64c
Alex Mikheev <alex.mikheev@beauhurst.com>
2025-10-30 15:10:29 +0000

    (fix) replace custom buffer picker with telescope built-in for cleaner delete functionality

--- init.lua
+++ init.lua
@@ -449,41 +449,8 @@ require('lazy').setup({
 
 			-- [[ Configure Telescope ]]
 			-- See `:help telescope` and `:help telescope.setup()`
-			local builtin = require 'telescope.builtin'
-			local action_state = require 'telescope.actions.state'
-			local actions = require 'telescope.actions'
-			local buffer_searcher
-
-			-- Custom function for buffers that handles deletion and refreshes the list
-			buffer_searcher = function()
-				builtin.buffers {
-					sort_mru = true,
-					ignore_current_buffer = true,
-					show_all_buffers = false,
-					attach_mappings = function(prompt_bufnr, map)
-						local refresh_buffer_searcher = function()
-							actions.close(prompt_bufnr)
-							-- Use vim.schedule to ensure the picker starts only after the previous one is fully closed
-							vim.schedule(buffer_searcher)
-						end
-
-						local delete_buf_and_stay = function()
-							local selection = action_state.get_selected_entry()
-							if selection and selection.bufnr then
-								-- Use nvim_buf_delete to force deletion and then refresh the picker
-								vim.api.nvim_buf_delete(selection.bufnr, { force = true })
-								refresh_buffer_searcher()
-							end
-						end
-
-						-- Delete single buffer on <C-d> and refresh the list to stay in the picker
-						map('n', '<C-d>', delete_buf_and_stay)
-						map('i', '<C-d>', delete_buf_and_stay)
-
-						return true
-					end,
-				}
-			end
+			local telescope = require('telescope')
+			local actions = require('telescope.actions')
 
 			require('telescope').setup {
 				-- You can put your default mappings / updates / etc. in here
@@ -507,6 +474,12 @@ require('lazy').setup({
 				pickers = {
 					find_files = {
 						hidden = true,
+					},
+					buffers = {
+						mappings = {
+							i = { ['<c-d>'] = actions.delete_buffer },
+							n = { ['<c-d>'] = actions.delete_buffer },
+						},
 					},
 				},
 				extensions = {
@@ -532,7 +505,7 @@ require('lazy').setup({
 			vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[S]earch [R]esume' })
 			vim.keymap.set('n', '<leader>f.', builtin.oldfiles,
 				{ desc = '[S]earch Recent Files ("." for repeat)' })
-			vim.keymap.set('n', '<leader><leader>', buffer_searcher, { desc = '[ ] Find existing buffers' })
+			vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
 			-- Git pickers
 			vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = '[G]it [C]ommits' })
