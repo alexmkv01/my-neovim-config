b261499b7d414b02dd14f2d7807a992084045cee
Alex Mikheev <alex.mikheev@beauhurst.com>
2025-11-17 10:19:29 +0000

    (feat) add leap.lua navigation plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -66,6 +66,7 @@ return {
   require "custom.plugins.breadcrumbs",
   require "custom.plugins.persistence",
   require "custom.plugins.markview",
+  require 'custom.plugins.leap',
 
   {
     'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
